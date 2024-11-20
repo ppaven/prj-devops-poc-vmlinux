b82ac0293f1826a7f8e48b372120e12f23136098
Philippe PAVEN <philippe.paven@easyteam.fr>
2024-11-20 17:39:01 +0100

    Test Build #4

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "azccinfratfback"
     container_name       = "tfstate"
-    # key                  = "poc.vml.tfstate"
+     # key                  = "poc.vml.tfstate"
     use_oidc = true
   }
 }
